b9952ff66af5ce8a0517ccb0c6f2a67b80a2ae7f
Abid Mahmood <abid.mahmood@inotech.eu>
2020-09-08 13:13:25 +0200

    GITStartPoint

---
+++ InoGrid/InoGridItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InoGrid
+{
+    public class InoGridItem : ContentControl
+    {
+        public object Label
+        {
+            get { return base.GetValue(LabelProperty); }
+            set { base.SetValue(LabelProperty, value); }
+        }
+
+        static InoGridItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(InoGridItem), new FrameworkPropertyMetadata(typeof(InoGridItem)));
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(nameof(Label), typeof(object), typeof(InoGridItem), new FrameworkPropertyMetadata());
+
+    }
+}
---
+++ InotechStyling.Copy/InoStaticResourceExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InotechStyling.Copy
+{
+    public class InoStaticResourceExtension : System.Windows.StaticResourceExtension
+    {
+        public InoStaticResourceExtension()
+        {
+        }
+
+        public InoStaticResourceExtension(object resourceKey) : base(resourceKey)
+        {
+        }
+    }
+}
